a5d1652acbda414eacad28ec9dfa59adfb8f7a34
airat-vagapov <airlake@yandex.ru>
2025-05-04 13:30:08 +0300

    add error block to form

--- src/components/AddNewTask/AddNewTaskForm/AddNewTaskForm.tsx
+++ src/components/AddNewTask/AddNewTaskForm/AddNewTaskForm.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import Input from "@/ui/Input/Input";
 import Button from "@/ui/Button/Button";
 import SuccessBlock from "@/components/SuccessBlock/SuccessBlock";
+import ErrorBlock from "@/components/ErrorBlock/ErrorBlock";
 import Droplist from "@/ui/Droplist/Droplist";
 
 import { ITask } from "@/types/task"
@@ -22,6 +23,7 @@ type AddNewTaskFormValues = {
 
 const AddNewTaskForm = () => {
     const [taskIsAdded, setTaskIsAdded] = useState<boolean>(false);
+    const [taskAddError, setTaskAddError] = useState<boolean>(false);
 
     const addNewTask = async (task: ITask) => {
         const res = await axios.post("http://localhost:8080/task", task)
@@ -42,11 +44,14 @@ const AddNewTaskForm = () => {
 
             try {
                 const res = await addNewTask(values);
+                setTaskAddError(false);
                 setTaskIsAdded(true);
                 addNewTaskForm.resetForm();
 
             } catch (err) {
                 console.error(err);
+                setTaskIsAdded(false);
+                setTaskAddError(true);
             }
         },
     });
@@ -54,7 +59,7 @@ const AddNewTaskForm = () => {
     const { errors, touched } = addNewTaskForm;
     return (
         <div className="relative">
-            {!taskIsAdded && (
+            {!taskIsAdded && !taskAddError && (
                 <form
                     className="flex flex-col gap-5"
                     onSubmit={addNewTaskForm.handleSubmit}
@@ -80,16 +85,6 @@ const AddNewTaskForm = () => {
                         value={addNewTaskForm.values.description}
                         errorText={errors.description}
                     />
-                    {/* <Input
-                        label="Description"
-                        id="text"
-                        name="text"
-                        inptType="text"
-                        onChange={addNewTaskForm.handleChange}
-                        onBlur={addNewTaskForm.handleBlur}
-                        value={addNewTaskForm.values.priority}
-                        errorText={errors.priority}
-                    /> */}
 
                     <Droplist
                         id="priority"
@@ -109,9 +104,18 @@ const AddNewTaskForm = () => {
             {taskIsAdded && (
                 <SuccessBlock
                     title="New task succesfully added!"
-                    text="New task you can see on main page"
+                    text="You can see new task on main page"
                     actionText="Add another task"
                     action={() => setTaskIsAdded(false)}
+                />
+            )}
+
+            {taskAddError && (
+                <ErrorBlock
+                    title="Ops! Error!"
+                    text="Something went wrong. Please try again."
+                    actionText="Try again"
+                    action={() => addNewTaskForm.handleSubmit()}
                 />
             )}
         </div>
---
+++ src/components/ErrorBlock/ErrorBlock.tsx
@@ -0,0 +1,29 @@
+import Icon from "@/ui/Icon/Icon"
+import TextLink from "@/ui/TextLink/TextLink"
+import Title from "@/ui/Title/Title"
+
+type ErrorBlockProps = {
+    title: string;
+    text: string;
+    actionText?: string;
+    action?: () => void;
+};
+
+const ErrorBlock: React.FC<ErrorBlockProps> = ({ title, text, action, actionText }) => {
+    return (
+        <div className="flex flex-col items-center justify-center gap-4">
+            <Icon name={"error"} size={48} color={"errorRed"} />
+            <div className="flex flex-col items-center justify-center gap-2">
+                <Title>{title}</Title>
+                <p className="text-base">{text}</p>
+                {actionText &&
+                    <TextLink type="action" action={action}>
+                        {actionText}
+                    </TextLink>}
+
+            </div>
+        </div>
+    )
+}
+
+export default ErrorBlock;
