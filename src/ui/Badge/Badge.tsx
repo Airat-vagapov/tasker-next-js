7a2159c632a5dc760344d69877f9542ba27b2816
airat-vagapov <airlake@yandex.ru>
2025-05-14 20:26:19 +0300

    color add

--- src/ui/Badge/Badge.tsx
+++ src/ui/Badge/Badge.tsx
@@ -9,10 +9,10 @@ const Badge: React.FC<BadgeProps> = ({ children, color }) => {
     console.log(color)
     return (
         <>
-            <div className={`relative py-1 px-2 pl-5 bg-purple text-mainblack rounded-3xl w-fit
+            <div className={`relative py-1 px-2 pl-5  text-mainblack rounded-3xl w-fit
                 after:content-[''] after:absolute after:block after:top-1/2 after:-translate-y-1/2 after:left-2 
-                after:w-2 after:h-2 after:bg-purpleFull after:rounded-full after:z-10
-                ${color ? color : ''}
+                after:w-2 after:h-2  after:rounded-full after:z-10
+                ${color ? color : 'bg-purple after:bg-purpleFull'}
             `}>
                 <p>{children}</p>
             </div>
