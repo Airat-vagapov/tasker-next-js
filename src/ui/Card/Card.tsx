f451f33be16ae18f2fbebec64af5e2ad39c4e1f8
airat-vagapov <airlake@yandex.ru>
2026-01-24 18:03:17 +0300

    add title to card component

--- src/ui/Card/Card.tsx
+++ src/ui/Card/Card.tsx
@@ -1,8 +1,9 @@
 type CardProps = {
+    title?: string;
     children: React.ReactNode;
 }
 
-const Card: React.FC<CardProps> = ({ children }) => {
+const Card: React.FC<CardProps> = ({ title, children }) => {
     return (
         <div className="bg-lightblack rounded-xl p-8">{children}</div>
     )
