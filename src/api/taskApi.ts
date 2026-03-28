f632a7c98098cec17e260e404b8bc4a9e35cbd50
airat-vagapov <airlake@yandex.ru>
2026-03-28 15:52:10 +0300

    update task api with httpClient

--- src/api/taskApi.ts
+++ src/api/taskApi.ts
@@ -1,14 +1,12 @@
 import { ITask, INewTaskData } from "@/types/task";
 import { getAllTasksParams } from '@/api/tasks.types'
 import { handleApiError } from "@/api/apiErrorHandler"
-import axios from "axios";
+import { httpClient } from "@/api/httpClient";
 
 interface ApiResponse {
     status: string;
     result: ITask[];
 }
-
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
 export const taskApi = {
     getAllTasks: async ({ task_id, search, status, sortBy, order = 'desc', priority }: getAllTasksParams) => {
@@ -16,7 +14,7 @@ export const taskApi = {
             params: { task_id, search, status, sortBy, order, priority },
         }
         try {
-            const { data } = await axios.get<ApiResponse>(`${BASE_URL}/tasks`, payload)
+            const { data } = await httpClient.get<ApiResponse>(`/tasks`, payload)
             return data.result
         } catch (error) {
             handleApiError(error)
@@ -25,8 +23,7 @@ export const taskApi = {
 
     getActiveTasks: async () => {
         try {
-            const { data } = await axios
-                .get<ApiResponse>(`${BASE_URL}/tasks/status?id=1,2`)
+            const { data } = await httpClient.get<ApiResponse>(`/tasks/status?id=1,2`)
             return data.result
         } catch (error) {
             handleApiError(error)
@@ -36,8 +33,7 @@ export const taskApi = {
 
     getDoneTasks: async () => {
         try {
-            const { data } = await axios
-                .get<ApiResponse>(`${BASE_URL}/tasks/status?id=3`)
+            const { data } = await httpClient.get<ApiResponse>(`/tasks/status?id=3`)
             return data.result
         } catch (error) {
             handleApiError(error)
@@ -47,7 +43,7 @@ export const taskApi = {
 
     addTask: async (task: INewTaskData) => {
         try {
-            const res = await axios.post(`${BASE_URL}/task`, task)
+            const res = await httpClient.post(`/task`, task)
             return res
         } catch (error) {
             handleApiError(error)
@@ -58,7 +54,7 @@ export const taskApi = {
 
         (id)
         try {
-            const res = await axios.delete(`${BASE_URL}/task/${id}`)
+            const res = await httpClient.delete(`/task/${id}`)
             return res
         } catch (error) {
             handleApiError(error)
@@ -67,7 +63,7 @@ export const taskApi = {
 
     changeTaskStatus: async (task: ITask) => {
         try {
-            const res = await axios.post(`${BASE_URL}/task/${task.id}`, { task })
+            const res = await httpClient.post(`/task/${task.id}`, { task })
             return res.data
         } catch (error) {
             handleApiError(error)
@@ -76,7 +72,7 @@ export const taskApi = {
 
     getTaskById: async (id: string) => {
         try {
-            const res = await axios.get(`${BASE_URL}/task/${id}`)
+            const res = await httpClient.get(`/task/${id}`)
             return res
         } catch (error) {
             handleApiError(error)
